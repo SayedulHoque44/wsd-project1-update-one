1cfee1637b1329aacfed6b69c89948dc669f4ed3
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-04-30 18:15:37 +0600

    bug fix

--- src/app/(withLayoutC)/c/transactions/page.tsx
+++ src/app/(withLayoutC)/c/transactions/page.tsx
@@ -1,18 +1,23 @@
-import Progress from "@/components/transition/Progress";
+import { Progress } from "antd";
 
 const Transactions = () => {
   return (
-    <div className="flex h-[calc(100vh-88px)] w-full  items-center justify-center text-[#1C2434] p-4 md:p-6 2xl:p-10">
+    <div className="flex h-[calc(100vh-88px)] w-full  items-center justify-center p-4 text-[#1C2434] md:p-6 2xl:p-10">
       <div className="flex items-center justify-center gap-5">
-        <div className="rounded-xl bg-slate-300 p-2">
+        <div className="rounded-xl bg-white  p-2">
           <h1 className="text-center">1.0000</h1>
           <p>Total Withdraw money</p>
         </div>
         <div>
-          <Progress popularity={"85000.50"} />
-          <h1 className="text-2xl font-bold">Withdraw</h1>
+          <Progress
+            strokeWidth={10}
+            strokeColor={"#1C2434"}
+            type="circle"
+            percent={20}
+          />
+          <h1 className="mt-3 text-2xl font-bold dark:text-white">Withdraw</h1>
         </div>
-        <div className="rounded-xl bg-slate-300 p-2">
+        <div className="rounded-xl bg-white p-2">
           <h1 className="text-center">1.0000</h1>
           <p>Total Pending request money</p>
         </div>
