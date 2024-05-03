87a6b48cc672adb68d5333cd7a395a71469f818f
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-05-03 15:37:33 +0600

    Live chat completed

--- src/redux/fetures/liveChat/chatPartnerSlice.ts
+++ src/redux/fetures/liveChat/chatPartnerSlice.ts
@@ -9,26 +9,35 @@ const initialState = {
     lastMesg: "hello",
     status: "request",
     messages: [
-      { id: "msg1", sender: "John Doe", text: "Hey, how's it going?" },
+      {
+        id: "msg1",
+        sender: "John Doe",
+        text: "Hey, how's it going?",
+        status: "default",
+      },
       {
         id: "msg2",
         sender: "You",
         text: "Hi! I'm doing well, thanks. How about you?",
+        status: "default",
       },
       {
         id: "msg3",
         sender: "John Doe",
         text: "I'm good too. What have you been up to lately?",
+        status: "default",
       },
       {
         id: "msg4",
         sender: "You",
         text: "Just been busy with work and some personal projects.",
+        status: "default",
       },
       {
         id: "msg5",
         sender: "John Doe",
         text: "I might go see a movie with friends. Do you have any movie recommendations?",
+        status: "default",
       },
     ],
   },
@@ -42,11 +51,18 @@ const chatPartnerSlice = createSlice({
     },
     removeMessage: (state, action) => {
       const { messageId } = action.payload;
-      state.partner.messages = state.partner.messages.filter(
-        (msg) => msg.id !== messageId,
-      );
+      state.partner.messages = state.partner.messages.map((message) => {
+        if (message.id === messageId) {
+          return {
+            ...message,
+            status: "deleted",
+          };
+        }
+        return message;
+      });
     },
   },
 });
+
 export const { startChat, removeMessage } = chatPartnerSlice.actions;
 export default chatPartnerSlice.reducer;
